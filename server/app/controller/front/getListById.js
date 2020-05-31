0c983226319f291599880a5f13920ea9fe6eb33e
yuluohuangquan <648779925@qq.com>
2020-06-01 00:09:24 +0800

    根据类别ID获得文章列表

---
+++ server/app/controller/front/getListById.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+/**
+ * 根据类别ID获得文章列表
+ */
+class MainController extends Controller {
+    async index() {
+        let id = parseInt(this.ctx.params.id)
+        if (id) {
+            let sql = 'SELECT article.id as id,' +
+                'article.title as title,' +
+                'article.introduce as introduce,' +
+                "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime," +
+                'article.view_count as view_count ,' +
+                'type.typeName as typeName ' +
+                'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+                'WHERE type_id=' + id
+            const result = await this.app.mysql.query(sql)
+            this.ctx.body = { data: result }
+        } else {
+            this.ctx.body = { data: '错误的Id' }
+        }
+    }
+}
+
+module.exports = MainController;
--- server/app/router/front.js
+++ server/app/router/front.js
@@ -5,4 +5,5 @@ module.exports = app => {
   router.get('/front/test', controller.front.home.index);
   router.get('/front/getArticleList', controller.front.getArticleList.index);
   router.get('/front/getArticleById/:id', controller.front.getArticleById.index);
+  router.get('/front/getListById/:id', controller.front.getListById.index);
 };
