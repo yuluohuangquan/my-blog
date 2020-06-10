127dc612a36a7a0e16fd78baa91add881ae637c4
z004069p <yu.sun.ext@siemens.com>
2020-06-10 18:12:01 +0800

    数据库导入数据

--- server/config/config.default.js
+++ server/config/config.default.js
@@ -10,7 +10,7 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {});
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1589852470709_350';
@@ -25,15 +25,23 @@ module.exports = appInfo => {
 
   config.mysql = {
     client: {
-      host: 'localhost',
+      host: '138.128.218.122',
       port: '3306',
       user: 'root',
-      password: '12345678',
+      password: '0ae7dafdfa08b4d4',
       database: 'react_blog',
     },
     app: true,
     agent: false,
   };
+
+  // client: {
+  //   host: 'localhost',
+  //   port: '3306',
+  //   user: 'sunyu',
+  //   password: '12345678',
+  //   database: 'react_blog',
+  // },
 
   config.security = {
     scrf: {
