d7f0bcd0f5fd0bd9f13ca9dfc99fde0965110d62
yuluohuangquan <648779925@qq.com>
2020-06-16 18:00:52 +0800

    获取用户列表接口

--- admin/src/components/userList/main.tsx
+++ admin/src/components/userList/main.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Container from "../container";
-import { Table } from 'antd';
-import { columns, data } from './constant';
+import { Table, Button } from "antd";
+import { columns, data } from "./constant";
 import css from "./styles/main.module.less";
 
 export default function Main() {
-    return (
-        <Container>
-            <div className={css.main}>
-                <div className={css.mainContent}>{"用户管理"}</div>
-                <Table columns={columns} dataSource={data} />
-            </div>
-        </Container>
-    );
+  const addUser = () => {
+    window.console.log("加");
+  };
+
+  return (
+    <Container>
+      <div className={css.main}>
+        <div className={css.mainContent}>
+          {"用户管理"}
+          <Button type="primary" onClick={addUser}>{"新增"}</Button>
+        </div>
+        <Table columns={columns} dataSource={data} />
+      </div>
+    </Container>
+  );
 }
