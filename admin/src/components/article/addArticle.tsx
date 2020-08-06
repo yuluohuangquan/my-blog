5573908a0ba49b1915670cd559fa6051a99a064c
yuluohuangquan <648779925@qq.com>
2020-08-06 18:04:08 +0800

    新增文章页面

--- admin/src/components/article/addArticle.tsx
+++ admin/src/components/article/addArticle.tsx
@@ -1,37 +1,71 @@
 import React, { useState } from "react";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 import { useHistory } from "react-router-dom";
-import { Input } from 'antd';
+import { Input, Button, Modal } from "antd";
 import css from "./styles/add.module.less";
 
 const { TextArea } = Input;
 
 export default function Main() {
-    let history = useHistory();
-    const [text, setText] = useState('');
+  let history = useHistory();
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [visible, setVisible] = useState(false);
 
-    const setValue = (e: any) => {
-        setText(e.target.value);
-    }
+  const setValue = (e: any) => {
+    setText(e.target.value);
+  };
 
-    const goBack = () => {
-        history.goBack();
-    }
+  const goBack = () => {
+    history.goBack();
+  };
 
-    return (
-        <div className={css.main}>
-            <div className={css.title}>
-                <h4>{"新增文章"}</h4>
-                <a onClick={goBack}>{'返回'}</a>
-            </div>
-            <section>
-                <div className={css.leftContent}>
-                    <TextArea value={text} onChange={setValue} />
-                </div>
-                <div className={css.rightContent}>
-                    <ReactMarkdown source={text} />
-                </div>
-            </section>
+  const saveDraft = () => {
+    // 保存为草稿
+  };
+
+  const changeVisible = () => {
+    // 打开关闭模态框
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <div className={css.main}>
+        <div className={css.title}>
+          <Input
+            className={css.titleInput}
+            placeholder={"文章名"}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className={css.titleContent}>
+            <Button>{"保存草稿"}</Button>
+            <Button onClick={changeVisible}>{"保存"}</Button>
+            <a onClick={goBack}>{"返回"}</a>
+          </div>
         </div>
-    );
+        <section>
+          <div className={css.leftContent}>
+            <TextArea value={text} onChange={setValue} />
+          </div>
+          <div className={css.rightContent}>
+            <ReactMarkdown source={text} />
+          </div>
+        </section>
+      </div>
+      {visible && (
+        <Modal
+          width="520px"
+          keyboard={false}
+          visible={visible}
+          title={"文章保存"}
+          destroyOnClose={true}
+          onCancel={changeVisible}
+        >
+          {"asd"}
+        </Modal>
+      )}
+    </>
+  );
 }
